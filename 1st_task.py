8309c6d1876cac7ace258139c5adf0d3f9406eed
Ivan-Menshov <93650472+Ivan-Menshov@users.noreply.github.com>
2021-11-12 16:56:29 +0300

    Delete 1st_task.py

--- 1st_task.py
+++
@@ -1,7 +0,0 @@
-number_int = 5
-number_float = 5.5
-string_1 = 'Hello'
-print(number_float, number_int, string_1, sep='\n')
-name = str(input('Привет. Как тебя зовут? '))
-age = int(input('А сколько тебе лет? '))
-print(f'Добро пожаловать {name}.Я запомню,что тебе уже {age}.')
